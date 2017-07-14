72320b99e39610d83c0a6b5c7111b5b4953063ee
Christian Moellnitz <cmolln10@student.aau.dk>
2017-07-14 09:06:25 +0200

    Old code in a new project.

---
+++ Mulinia/Assets/Scripts/TurnSpriteX.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnSpriteX : MonoBehaviour
+{
+    float lastX;
+
+    // Use this for initialization
+    void Start()
+    {
+        lastX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 scale = transform.localScale;
+
+        if(lastX < transform.position.x)
+        {
+            if(scale.x < 0)
+            {
+                scale.x *= -1;
+            }
+        }
+        else if (lastX > transform.position.x)
+        {
+            if (scale.x > 0)
+            {
+                scale.x *= -1; 
+            }
+        }
+
+        transform.localScale = scale;
+
+        lastX = transform.position.x;
+    }
+}
