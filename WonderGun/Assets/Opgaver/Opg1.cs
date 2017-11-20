0ba14a6262b846c22f4413528b4b2e3b4b87e528
Unknown <cmolln10@student.aau.dk>
2017-11-20 14:19:13 +0100

    Wondergun projekt

---
+++ WonderGun/Assets/Opgaver/Opg1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opg1 : MonoBehaviour {
+
+    string opgaveStreng = "Hejsa!";
+
+    /// <summary>
+    /// Opgave!
+    /// Få unity til at skrive at Din besked er: Pizza
+    /// </summary>
+	void Start () {
+        opgaveStreng = "Pizza!";
+        print("Din besked er: Pizza");
+        print("Din besked er: " + opgaveStreng); // <--
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
